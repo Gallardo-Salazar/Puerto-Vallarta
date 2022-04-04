5e71c1dae535f14f5854d7581f03f0b66ccc29e6
unknown <gallardoirg@gmail.com>
2022-04-04 12:39:56 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 37812176,
+    "boundingBox": {
+        "lx": -150826.6034082031,
+        "ly": 2294656.2263464357,
+        "lz": -23.616625671386719,
+        "ux": -150416.31091247558,
+        "uy": 2295066.518842163,
+        "uz": 386.67587005615237
+    },
+    "tightBoundingBox": {
+        "lx": -150826.6034,
+        "ly": 2294656.2263,
+        "lz": -23.616600000000003,
+        "ux": -150497.6295,
+        "uy": 2295066.5188,
+        "uz": 40.0405
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.553237199783325,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
